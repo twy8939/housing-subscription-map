bf90eaa7b969195941310760cd781b74e1a8325e
twy8939 <twy8939@naver.com>
2024-06-11 22:49:09 +0900

    feat : header 추가

---
+++ app/_widgets/header/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header({ rightContent }: IHeaderProps) {
+  return (
+    <header className="sticky top-0 left-0 w-full h-12 px-3 flex justify-between z-50 border-b items-center">
+      <div className="flex">
+        <Link href="/">Home</Link>
+      </div>
+      {rightContent && <div>{rightContent}</div>}
+    </header>
+  );
+}

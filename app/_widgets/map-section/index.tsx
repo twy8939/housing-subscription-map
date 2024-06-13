d6ae11e17d3e70a38ed06c84c8367d5b1b7b1ceb
twy8939 <twy8939@naver.com>
2024-06-13 22:15:41 +0900

    fix : swr 제거

--- app/_widgets/map-section/index.tsx
+++ app/_widgets/map-section/index.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import Map from "@/app/_features/map";
-import useMap from "@/app/_hooks/useMap";
 import React from "react";
 
 export default function MapSection() {
-  const { initializeMap } = useMap();
-
-  const onLoadMap = (map: NaverMap) => {
-    initializeMap(map);
-  };
-
   return (
     <>
-      <Map onLoad={onLoadMap} />
+      <Map />
     </>
   );
 }

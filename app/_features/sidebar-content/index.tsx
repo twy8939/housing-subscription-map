66549e554c2b43359dfc38b7d47b97e8604fb452
twy8939 <twy8939@naver.com>
2024-06-17 22:41:57 +0900

    feat : tailwind scrollbar Add

--- app/_features/sidebar-content/index.tsx
+++ app/_features/sidebar-content/index.tsx
@@ -5,7 +5,7 @@ import ApplyHomeList from "../applyhome-list";
 
 export default function SidebarContent() {
   return (
-    <div className="flex-1 overflow-hidden overflow-y-scroll">
+    <div className="flex-1 overflow-x-hidden overflow-y-scroll scrollbar-thin">
       <div className="border-b px-6 py-5">
         <LocationButton />
         <LocationWeather />

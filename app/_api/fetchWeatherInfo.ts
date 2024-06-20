ace352dde38502163ac48a56361e4b610718e0fc
twy8939 <twy8939@naver.com>
2024-06-20 22:12:03 +0900

    feat : TemperatureDifference Add

--- app/_api/fetchWeatherInfo.ts
+++ app/_api/fetchWeatherInfo.ts
@@ -1,6 +1,6 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
-export const fetchWeatherInfo = async () => {
+export const fetchWeatherInfo = async (date: Dayjs = dayjs()) => {
   const baseUrl =
     "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
   const serviceKey = process.env.NEXT_PUBLIC_WEATHER_SERVICE_KEY as string;
@@ -10,8 +10,8 @@ export const fetchWeatherInfo = async () => {
     numOfRows: "50",
     pageNo: "1",
     dataType: "JSON",
-    base_date: dayjs().format("YYYYMMDD"),
-    base_time: dayjs().subtract(30, "minute").format("HHmm"),
+    base_date: dayjs(date).format("YYYYMMDD"),
+    base_time: dayjs().subtract(1, "hour").format("HHmm"),
     nx: "55",
     ny: "127",
   };
--- app/_entities/location-weather/index.tsx
+++ app/_entities/location-weather/index.tsx
@@ -2,19 +2,34 @@ import { fetchWeatherInfo } from "@/app/_api/fetchWeatherInfo";
 import React from "react";
 import { find } from "lodash";
 import { SKY_CODE } from "./constants";
+import dayjs from "dayjs";
 
 export default async function LocationWeather() {
-  const data = await fetchWeatherInfo();
+  const yesterday = dayjs().subtract(1, "day");
 
-  const air = find(data, ["category", "SKY"]);
-  const temper = find(data, ["category", "T1H"]);
+  const todayWeather = await fetchWeatherInfo();
+  const yesterdayWeather = await fetchWeatherInfo(yesterday);
+
+  const air = Number(find(todayWeather, ["category", "SKY"])?.fcstValue);
+  const temper = Number(find(todayWeather, ["category", "T1H"])?.fcstValue);
+
+  const yesterdayTemper = Number(
+    find(yesterdayWeather, ["category", "T1H"])?.fcstValue
+  );
+
+  const temperatureDifference = temper - yesterdayTemper;
 
   return (
     <div className="text-xs flex gap-2 my-1">
       <div className="flex gap-1">
-        <span>{air?.fcstValue ? SKY_CODE?.[air.fcstValue].label : "-"}</span>
-        <span className="font-bold">{temper?.fcstValue}˚</span>
-        <span>어제보다 1℃ 낮아요</span>
+        <span>{air ? SKY_CODE?.[air].label : "-"}</span>
+        <span className="font-bold">{temper}˚</span>
+        <span>
+          어제보다 {temperatureDifference}℃
+          {temperatureDifference && temperatureDifference > 0
+            ? " 높아요"
+            : " 낮아요"}
+        </span>
       </div>
       <div className="flex gap-1">
         <span>

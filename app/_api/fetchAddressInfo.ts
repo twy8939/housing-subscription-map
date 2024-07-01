23744da445307ab8533618540d0fa89291f94f64
twy8939 <twy8939@naver.com>
2024-07-01 22:57:17 +0900

    fix : Naver API > Cors 해결

--- app/_api/fetchAddressInfo.ts
+++ app/_api/fetchAddressInfo.ts
@@ -6,7 +6,16 @@ export const fetchAddressInfo = async ({
   lon: number;
 }) => {
   try {
-    const res = await fetch(`/api/geocode?lat=${lat}&lon=${lon}`);
+    const res = await fetch(
+      `http://localhost:3000/example/map-reversegeocode/v2/gc?coords=128.12345,37.98776`,
+      {
+        headers: {
+          "X-NCP-APIGW-API-KEY-ID": process.env.NEXT_PUBLIC_NCP_CLIENT_ID || "",
+          "X-NCP-APIGW-API-KEY":
+            process.env.NEXT_PUBLIC_NCP_CLIENT_SECRET || "",
+        },
+      }
+    );
 
     if (!res.ok) {
       throw new Error(`Error: ${res.status}`);
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                destination: 'https://naveropenapi.apigw.ntruss.com/:path*',
+                source: '/example/:path*',
+            },
+        ]
+    }
+};
 
 export default nextConfig;
